fa2289eac500bb8c5915dfa3d2f48fe62d4a583b
Ann <55690276+annafanaseva@users.noreply.github.com>
2024-02-14 14:29:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Main from './assets/img/main.png';
 import './App.css';
 
 function App() {
+
+  const daysLeftNewYear = () => {
+    const date = new Date();
+    const nextNewYearDate = new Date('December 31, 2024');
+    const res = ((nextNewYearDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24);
+    return Math.round(res);
+  }
+
+  console.log(daysLeftNewYear());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-background">
+      </div>
+      <div className="wrapper">
+        <img className="wrapper-img" src={Main} />
+        <div className="wrapper-text">
+          <p>До WOW эффекта осталось: {daysLeftNewYear()} дней</p>
+        </div>
+      </div>
     </div>
   );
 }

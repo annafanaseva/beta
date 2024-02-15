f9507db29bb6a315fb3ce5a9462f4d284ffdb61c
Ann <55690276+annafanaseva@users.noreply.github.com>
2024-02-15 18:50:03 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Chart } from "react-google-charts";
 import Main from './assets/img/main.png';
 import './App.css';
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Факт", 112],
+  ["Осталось", 12],
+];
 
+export const options = {
+  is3D: true,
+  backgroundColor: 'transparent',
+  slices: {
+    0: { color: "#3F2BBE" },
+    1: { color: "#E15897" },
+  },
+};
+
+const App = () => {
   const daysLeftNewYear = () => {
     const date = new Date();
     const nextNewYearDate = new Date('December 31, 2024');
@@ -10,7 +25,6 @@ function App() {
     return Math.round(res);
   }
 
-  console.log(daysLeftNewYear());
   return (
     <div className="app">
       <div className="app-background">
@@ -19,6 +33,14 @@ function App() {
         <img className="wrapper-img" src={Main} />
         <div className="wrapper-text">
           <p>До WOW эффекта осталось: {daysLeftNewYear()} дней</p>
+
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"300px"}
+          />
         </div>
       </div>
     </div>
